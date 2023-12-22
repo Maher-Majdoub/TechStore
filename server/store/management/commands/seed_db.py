30770c56a351e674f3766713c5f4f88e2e07d030
Maher-Majdoub <maher.majjdoub@gmail.com>
2023-12-22 04:13:55 +0100

    added command to seed the data base

---
+++ server/store/management/commands/seed_db.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+from django.db import connections
+import os
+
+class Command(BaseCommand):
+    help = 'Seed the database'
+
+    def run(self, script_path):
+        with open(script_path, 'r') as file:
+            sql_statements = file.read()
+            with connections['default'].cursor() as cursor:
+                try:
+                    cursor.execute(sql_statements)
+                    self.stdout.write(self.style.SUCCESS("Database seeded successfully."))
+                except Exception as e:
+                    self.stderr.write(self.style.ERROR(f"Error seeding database: {e}"))
+
+    def handle(self, *args, **options):
+        curr = os.path.dirname(os.path.realpath(__file__))
+        print('seeding users.....')
+        self.run(os.path.join(curr, 'sql/seed_users.sql'))
+        print('seeding categories.....')
+        self.run(os.path.join(curr, 'sql/seed_categories.sql'))
+        print('seeding variations.....')
+        self.run(os.path.join(curr, 'sql/seed_variations.sql'))
+
+
+        
