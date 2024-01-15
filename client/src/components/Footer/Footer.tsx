e47bcaaedac5cface1ef478cdd1a2ba4e61eb415
Maher-Majdoub <maher.majjdoub@gmail.com>
2024-01-15 23:45:43 +0100

    applied design to the footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -2,18 +2,28 @@ import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <div className="flx-bx">
-        <div>
+    <footer
+      className={styles.footer}
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log("subscribe");
+      }}
+    >
+      <div className={styles.newsLister + " container"}>
+        <div className={styles.title}>
           <h2>Sign Up To Our Newslteller.</h2>
           <p>Be the first to hear about the latest offers.</p>
         </div>
-        <form className="flx-bx">
-          <input type="text" placeholder="Your Email" />
-          <button>Subscribe</button>
+        <form className={styles.frm}>
+          <input
+            type="text"
+            placeholder="Your Email"
+            className={styles.input}
+          />
+          <button className={styles.btn}>Subscribe</button>
         </form>
       </div>
-      <div>
+      <div className={styles.copyright + " container"}>
         <p>Copyright &copy; 2024-present TechStore, Inc. All rights reserved</p>
       </div>
     </footer>
