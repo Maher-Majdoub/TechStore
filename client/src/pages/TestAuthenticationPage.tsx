53ab2a6496037e7892070d5e51609b38013ad9c3
Maher-Majdoub <maher.majjdoub@gmail.com>
2024-01-21 22:45:18 +0100

    show customer info when is logged in

--- client/src/pages/TestAuthenticationPage.tsx
+++ client/src/pages/TestAuthenticationPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import useCustomer from "../hooks/useCustomer";
 import { useNavigate } from "react-router-dom";
+import ProductCard from "../components/ProductCard/ProductCard";
 
 const TestAuthenticationPage = () => {
   const navigate = useNavigate();
@@ -13,10 +14,24 @@ const TestAuthenticationPage = () => {
   return (
     <main>
       {isLoading && <p>Loding...</p>}
-      <div>
-        <h1>{customer?.user.email}</h1>
-        <p>yeah we are logged in</p>
-      </div>
+      {customer && (
+        <>
+          <div>
+            <h1>{customer.user.email}</h1>
+            <p>{customer.user.first_name}</p>
+            <p>{customer.user.last_name}</p>
+            <p>yeah we are logged in</p>
+          </div>
+          <div>
+            <h1>wishes</h1>
+            <ul>
+              {customer.wish_list.map((wish) => (
+                <ProductCard key={wish.id} product={wish.product} />
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
     </main>
   );
 };
