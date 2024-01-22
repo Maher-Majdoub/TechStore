6a51306e4984517ad48fd947724363fd59eb50bb
Maher-Majdoub <maher.majjdoub@gmail.com>
2024-01-22 23:51:05 +0100

    show real cart items in miniCart component

--- client/src/components/Minicart/Minicart.tsx
+++ client/src/components/Minicart/Minicart.tsx
@@ -1,42 +1,55 @@
-import useProducts from "../../hooks/useProducts";
+import useCart from "../../hooks/useCart";
 import Button from "../Button/Button";
 import MiniProductCard from "../MiniProductCart/MiniProductCard";
 import styles from "./Minicart.module.css";
 
-const total = 1234.99;
-
 const Minicart = () => {
-  const { data } = useProducts({ config: { params: { page: 1 } } });
+  const { cart, isLoading, isError, deleteFromCart } = useCart();
 
-  const products = data?.results.slice(0, 3);
+  if (isError) {
+    return <h1>Something Went Wrong</h1>;
+  }
+
+  var total = 0;
+  if (cart?.items)
+    for (const item of cart.items) {
+      total += item.product.unit_price * item.quantity;
+    }
 
   return (
-    <div className={styles.cart}>
-      <h3 className={styles.title}>My Cart</h3>
-      <span className={styles.subTitle}>{products?.length} items in cart</span>
-      <div className={styles.container}>
-        <Button>View or Edit Your Cart</Button>
+    <>
+      {isLoading}
+      <div className={styles.cart}>
+        <h3 className={styles.title}>My Cart</h3>
+        <span className={styles.subTitle}>
+          {cart?.items.length} items in cart
+        </span>
+        <div className={styles.container}>
+          <Button>View or Edit Your Cart</Button>
+        </div>
+        <ul className={styles.productsList}>
+          {cart?.items?.map((item) => (
+            <li key={item.product.id} className={styles.product}>
+              <MiniProductCard
+                product={item.product}
+                count={item.quantity}
+                onDelete={() => {
+                  deleteFromCart({ itemId: item.id });
+                }}
+                onModify={() => {}}
+              />
+            </li>
+          ))}
+        </ul>
+        <div className={styles.subTotal}>
+          <span>Subtotal: </span>
+          <span>${total.toFixed(2)}</span>
+        </div>
+        <div className={styles.container}>
+          <Button>Go To Checkout</Button>
+        </div>
       </div>
-      <ul className={styles.productsList}>
-        {products?.map((prod) => (
-          <li key={prod.id} className={styles.product}>
-            <MiniProductCard
-              product={prod}
-              count={5}
-              onDelete={() => {}}
-              onModify={() => {}}
-            />
-          </li>
-        ))}
-      </ul>
-      <div className={styles.subTotal}>
-        <span>Subtotal: </span>
-        <span>${total}</span>
-      </div>
-      <div className={styles.container}>
-        <Button>Go To Checkout</Button>
-      </div>
-    </div>
+    </>
   );
 };
 
