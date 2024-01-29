d1ad7ae6e5068596bda5f98407d6551e697d146c
Maher-Majdoub <maher.majjdoub@gmail.com>
2024-01-29 22:28:51 +0100

    implemented account dashboard component

--- client/src/components/AccountDashboard/AccountDashboard.tsx
+++ client/src/components/AccountDashboard/AccountDashboard.tsx
@@ -1,5 +1,64 @@
+import useCustomer from "../../hooks/useCustomer";
+import styles from "./AccountDashboard.module.css";
+
 const AccountDashboard = () => {
-  return <h2>Account Dashboard</h2>;
+  const { customer } = useCustomer();
+  let hasDefaultShippingAdress = false;
+  return (
+    <>
+      {customer && (
+        <div className={styles.container}>
+          <div>
+            <div className={styles.titleContainer}>
+              <h2>Account Information</h2>
+            </div>
+            <div className={styles.sectionsContainer}>
+              <div className={styles.sectionContainer}>
+                <h4>Contact Information</h4>
+                <span>{`${customer.user.first_name} ${customer.user.last_name}`}</span>
+                <span>{customer.user.email}</span>
+                <div className={styles.flxBx}>
+                  <span className={styles.link}>Edit</span>
+                  <span className={styles.link}>Change Password</span>
+                </div>
+              </div>
+              <div className={styles.sectionContainer}>
+                <h4>Newsletters</h4>
+                <span>You are not subscribed to our newsletter</span>
+                <span className={styles.link}>Edit</span>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className={`${styles.titleContainer} ${styles.flxBx}`}>
+              <h2>Address Book</h2>
+              <span className={styles.link}>Manage Addresses</span>
+            </div>
+            <div className={styles.sectionsContainer}>
+              <div className={styles.sectionContainer}>
+                <h4>Default Billing Address</h4>
+                <span>You have not set a default billing address.</span>
+                <span className={styles.link}>Edit Address</span>
+              </div>
+              <div className={styles.sectionContainer}>
+                <h4>Default Shipping Address</h4>
+                {customer.adresses.map((address) => {
+                  if (address.is_default) {
+                    hasDefaultShippingAdress = true;
+                    return <span>{address.city}</span>;
+                  }
+                })}
+                {!hasDefaultShippingAdress && (
+                  <span>You have not set a default shipping address.</span>
+                )}
+                <span className={styles.link}>Edit Address</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default AccountDashboard;
--- client/src/pages/UserAccountPage/UserAccountPage.tsx
+++ client/src/pages/UserAccountPage/UserAccountPage.tsx
@@ -7,95 +7,99 @@ import Ordres from "../../components/Orders/Ordres";
 import WishList from "../../components/WishList/WishList";
 import CompareList from "../../components/CompareList/CompareList";
 import Navigator from "../../components/Navigator/Navigator";
+import LinksSection from "../../components/LinksSection/LinksSection";
 
 enum Section {
-  dashBoard,
-  info,
-  addresses,
-  orders,
-  wishList,
-  compareList,
+  dashBoard = "My Dashboard",
+  info = "Account Information",
+  addresses = "Address Book",
+  orders = "My Orders",
+  wishList = "My Wish List",
+  compareList = "Compare Products",
 }
 
 const UserAccountPage = () => {
-  const [selectedSection, setSelectedSection] = useState(Section.info);
+  const [selectedSection, setSelectedSection] = useState(Section.dashBoard);
 
   return (
     <>
-      <main className={styles.container + " container"}>
+      <main className=" container">
         <Navigator />
-        <h1 className={styles.title}>My Dashboard</h1>
-        <div className={styles.content}>
-          <ul className={styles.sectionSelectors}>
-            <li
-              className={`${styles.sectionSelector} ${
-                selectedSection === Section.dashBoard ? styles.selected : ""
-              }`}
-              onClick={() => {
-                setSelectedSection(Section.dashBoard);
-              }}
-            >
-              Account DashBoard
-            </li>
-            <li
-              className={`${styles.sectionSelector} ${
-                selectedSection === Section.info ? styles.selected : ""
-              }`}
-              onClick={() => {
-                setSelectedSection(Section.info);
-              }}
-            >
-              Account Information
-            </li>
-            <li
-              className={`${styles.sectionSelector} ${
-                selectedSection === Section.addresses ? styles.selected : ""
-              }`}
-              onClick={() => {
-                setSelectedSection(Section.addresses);
-              }}
-            >
-              Address Book
-            </li>
-            <li
-              className={`${styles.sectionSelector} ${
-                selectedSection === Section.orders ? styles.selected : ""
-              }`}
-              onClick={() => {
-                setSelectedSection(Section.orders);
-              }}
-            >
-              My Orders
-            </li>
-            <li
-              className={`${styles.sectionSelector} ${
-                selectedSection === Section.wishList ? styles.selected : ""
-              }`}
-              onClick={() => {
-                setSelectedSection(Section.wishList);
-              }}
-            >
-              My Wish List
-            </li>
-            <li
-              className={`${styles.sectionSelector} ${
-                selectedSection === Section.compareList ? styles.selected : ""
-              }`}
-              onClick={() => {
-                setSelectedSection(Section.compareList);
-              }}
-            >
-              Compare Products
-            </li>
-          </ul>
-          <div className={styles.section}>
-            {selectedSection === Section.dashBoard && <AccountDashboard />}
-            {selectedSection === Section.info && <AccountInformation />}
-            {selectedSection === Section.addresses && <Addresses />}
-            {selectedSection === Section.orders && <Ordres />}
-            {selectedSection === Section.wishList && <WishList />}
-            {selectedSection === Section.compareList && <CompareList />}
+        <div className={styles.container}>
+          <h1 className={styles.title}>{selectedSection}</h1>
+          <div className={styles.content}>
+            <ul className={styles.sectionSelectors}>
+              <li
+                className={`${styles.sectionSelector} ${
+                  selectedSection === Section.dashBoard ? styles.selected : ""
+                }`}
+                onClick={() => {
+                  setSelectedSection(Section.dashBoard);
+                }}
+              >
+                Account DashBoard
+              </li>
+              <li
+                className={`${styles.sectionSelector} ${
+                  selectedSection === Section.info ? styles.selected : ""
+                }`}
+                onClick={() => {
+                  setSelectedSection(Section.info);
+                }}
+              >
+                Account Information
+              </li>
+              <li
+                className={`${styles.sectionSelector} ${
+                  selectedSection === Section.addresses ? styles.selected : ""
+                }`}
+                onClick={() => {
+                  setSelectedSection(Section.addresses);
+                }}
+              >
+                Address Book
+              </li>
+              <li
+                className={`${styles.sectionSelector} ${
+                  selectedSection === Section.orders ? styles.selected : ""
+                }`}
+                onClick={() => {
+                  setSelectedSection(Section.orders);
+                }}
+              >
+                My Orders
+              </li>
+              <li
+                className={`${styles.sectionSelector} ${
+                  selectedSection === Section.wishList ? styles.selected : ""
+                }`}
+                onClick={() => {
+                  setSelectedSection(Section.wishList);
+                }}
+              >
+                My Wish List
+              </li>
+              <li
+                className={`${styles.sectionSelector} ${
+                  selectedSection === Section.compareList ? styles.selected : ""
+                }`}
+                onClick={() => {
+                  setSelectedSection(Section.compareList);
+                }}
+              >
+                Compare Products
+              </li>
+            </ul>
+            <div className={styles.section}>
+              {selectedSection === Section.dashBoard && <AccountDashboard />}
+              {selectedSection === Section.info && <AccountInformation />}
+              {selectedSection === Section.addresses && <Addresses />}
+              {selectedSection === Section.orders && <Ordres />}
+              {selectedSection === Section.wishList && <WishList />}
+              {selectedSection === Section.compareList && <CompareList />}
+            </div>
           </div>
+          <LinksSection />
         </div>
       </main>
     </>
