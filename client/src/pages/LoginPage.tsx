b0a6b07c6794f31456577a83a0c53f568d8a5586
Maher-Majdoub <maher.majjdoub@gmail.com>
2024-01-29 22:55:01 +0100

    implemented navigation to customer page when accessing to login page automaticly if user is already logged in

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,11 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button/Button";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import useLogin from "../hooks/useLogin";
 import Navigator from "../components/Navigator/Navigator";
+import { useJwt } from "react-jwt";
 
 const LoginPage = () => {
   const navigate = useNavigate();
+  const accessToken = localStorage.getItem("access_token");
+  if (accessToken) {
+    const { isExpired } = useJwt(accessToken);
+    if (!isExpired) {
+      useEffect(() => {
+        navigate("/customer");
+      }, []);
+    }
+  }
+
   const { login, data, isError } = useLogin();
   const username = useRef<HTMLInputElement>(null);
   const password = useRef<HTMLInputElement>(null);
@@ -21,67 +32,71 @@ const LoginPage = () => {
     <main>
       <Navigator />
       <h2>Customer Login</h2>
-      <div>
-        <h5>Registered Customers</h5>
-        <span>If you have an account, sign in with your credentials.</span>
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-          }}
-        >
+      {(!data || isError) && (
+        <>
           <div>
-            <span>User Name</span>
-            <input
-              ref={username}
-              type="text"
-              placeholder="Your User Name"
-              required
-            />
+            <h5>Registered Customers</h5>
+            <span>If you have an account, sign in with your credentials.</span>
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+              }}
+            >
+              <div>
+                <span>User Name</span>
+                <input
+                  ref={username}
+                  type="text"
+                  placeholder="Your User Name"
+                  required
+                />
+              </div>
+              <div>
+                <span>Password</span>
+                <input
+                  ref={password}
+                  type="password"
+                  placeholder="Your Password"
+                  required
+                />
+              </div>
+              <div>
+                <Button
+                  onClick={() => {
+                    if (username.current?.value && password.current?.value) {
+                      login({
+                        username: username.current.value,
+                        password: password.current.value,
+                      });
+                    }
+                  }}
+                  filled
+                >
+                  Sign In
+                </Button>
+              </div>
+            </form>
           </div>
           <div>
-            <span>Password</span>
-            <input
-              ref={password}
-              type="password"
-              placeholder="Your Password"
-              required
-            />
-          </div>
-          <div>
+            <h5>New Customer?</h5>
+            <span>Creating an account has many benefits:</span>
+            <ul>
+              <li>Check out faster</li>
+              <li>Exclusive offers and discounts</li>
+              <li>Save order history</li>
+              <li>Track orders and more</li>
+            </ul>
             <Button
               onClick={() => {
-                if (username.current?.value && password.current?.value) {
-                  login({
-                    username: username.current.value,
-                    password: password.current.value,
-                  });
-                }
+                navigate("/signup");
               }}
               filled
             >
-              Sign In
+              Create An Account
             </Button>
           </div>
-        </form>
-      </div>
-      <div>
-        <h5>New Customer?</h5>
-        <span>Creating an account has many benefits:</span>
-        <ul>
-          <li>Check out faster</li>
-          <li>Exclusive offers and discounts</li>
-          <li>Save order history</li>
-          <li>Track orders and more</li>
-        </ul>
-        <Button
-          onClick={() => {
-            navigate("/signup");
-          }}
-          filled
-        >
-          Create An Account
-        </Button>
-      </div>
+        </>
+      )}
     </main>
   );
 };
